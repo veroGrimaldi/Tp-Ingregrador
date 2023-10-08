6baafb6ffa2b247698ca02a69198d90a2c685731
Vero Grimaldi <naoexx_98@hotmail.com>
2023-10-08 18:53:11 -0300

    proceso compra

--- ticket/js/script.js
+++ ticket/js/script.js
@@ -1,10 +1,8 @@
 (() => {
     'use strict'
   
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.needs-validation')
   
-    // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
       form.addEventListener('submit', event => {
         if (!form.checkValidity()) {
@@ -24,13 +22,13 @@ function calcTotal() {
     let cant=document.getElementById("cant").value
     console.log("cant"+cant)
 
-    if (cant>=0) {
+    if (cant>=1) {
         let total=cant*precio
         console.log("total="+total)
 
         let desc=document.getElementById("desc").value
         total=total - (total*desc/100)
-        console.log("total con descuento"+total)
+        console.log("total con descuento="+total)
 
         document.getElementById("total").className="fw-bold p-1"
         document.getElementById("total").innerHTML=total
